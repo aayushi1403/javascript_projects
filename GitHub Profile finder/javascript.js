a37976ab99f8e05a9e3d936e8e6a864d721b77f2
aayushi1403 <aayushir685@gmail.com>
2026-07-08 23:56:10 +0530

    started the project Github profile finder

---
+++ GitHub Profile finder/javascript.js
@@ -0,0 +1,33 @@
+const searchbar=document.querySelector(".Search-bar")
+const searchglass=document.querySelector(".searchglass")
+const searchglass=document.querySelector(".searchglass")
+
+const profilepicture=document.querySelector(".profile-pic")
+const name=document.querySelector(".name")
+const bio=document.querySelector(".bio")
+const repository=document.querySelector(".repo")
+const followers=document.querySelector(".followers")
+
+const following=document.querySelector(".following")
+const location=document.querySelector(".location")
+const gitlink=document.querySelector(".github link")
+
+const blog=document.querySelector(".linked-in")
+const twitter=document.querySelector(".twitter")
+
+const repo1=document.querySelector(".repo-card1")
+const repo2=document.querySelector(".repo-card2")
+const repo3=document.querySelector(".repo-card2")
+const repo4=document.querySelector(".repo-card3")
+
+const star1=document.querySelector(".stars1")
+const fork1=document.querySelector(".forks1")
+
+const star2=document.querySelector(".stars2")
+const fork2=document.querySelector(".forks2")
+
+const star3=document.querySelector(".stars3")
+const fork3=document.querySelector(".forks3")
+
+const star4=document.querySelector(".stars4")
+const fork4=document.querySelector(".forks4")
