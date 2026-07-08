2e341856d1bb72832aa42a2f98e4fbd4a7b3e4f8
aayushi1403 <aayushir685@gmail.com>
2026-07-09 00:55:35 +0530

    Fetch and display GitHub user profile

--- GitHub Profile finder/javascript.js
+++ GitHub Profile finder/javascript.js
@@ -1,6 +1,5 @@
 const searchbar=document.querySelector(".Search-bar")
-const searchglass=document.querySelector(".searchglass")
-const searchglass=document.querySelector(".searchglass")
+const searchglass=document.querySelector(".searchinglass")
 
 const profilepicture=document.querySelector(".profile-pic")
 const name=document.querySelector(".name")
@@ -9,7 +8,7 @@ const repository=document.querySelector(".repo")
 const followers=document.querySelector(".followers")
 
 const following=document.querySelector(".following")
-const location=document.querySelector(".location")
+const userlocation=document.querySelector(".location") //mistake--->don't location as it conflicts with window location in broswer
 const gitlink=document.querySelector(".github link")
 
 const blog=document.querySelector(".linked-in")
@@ -31,3 +30,25 @@ const fork3=document.querySelector(".forks3")
 
 const star4=document.querySelector(".stars4")
 const fork4=document.querySelector(".forks4")
+
+const giturl="https://api.github.com/users/"
+const repourl="https://api.github.com/users/aayushi1403/repos"
+
+
+async function githubfind(username) {
+  const response = await fetch(giturl+username)
+  if(response.status==404){
+    alert("please put a valid username");
+    return;
+  }
+  else{
+    var data=await response.json();
+    profilepicture.src=data.avatar_url;
+    name.innerHTML=data.login;
+  }
+}
+
+
+searchglass.addEventListener('click',()=>{
+  githubfind(searchbar.value);
+})
