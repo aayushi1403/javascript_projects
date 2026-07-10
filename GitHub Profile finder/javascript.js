3836c20558eaff4de10ef80e4c8672fc0c4c48c7
aayushi1403 <aayushir685@gmail.com>
2026-07-10 23:14:51 +0530

    gitHub profile finder is completed

--- GitHub Profile finder/javascript.js
+++ GitHub Profile finder/javascript.js
@@ -5,19 +5,19 @@ const profilepicture=document.querySelector(".profile-pic")
 const name=document.querySelector(".name")
 const bio=document.querySelector(".bio")
 const repository=document.querySelector(".repo")
-const followers=document.querySelector(".followers")
+const userfollowers=document.querySelector(".followers")
 
-const following=document.querySelector(".following")
+const userfollowing=document.querySelector(".following")
 const userlocation=document.querySelector(".location") //mistake--->don't location as it conflicts with window location in broswer
-const gitlink=document.querySelector(".github link")
+const gitlink=document.querySelector(".github-link")
 
-const blog=document.querySelector(".linked-in")
-const twitter=document.querySelector(".twitter")
+const userblog=document.querySelector(".blog")
+const usertwitter=document.querySelector(".twitter")
 
 const repo1=document.querySelector(".repo-card1")
 const repo2=document.querySelector(".repo-card2")
-const repo3=document.querySelector(".repo-card2")
-const repo4=document.querySelector(".repo-card3")
+const repo3=document.querySelector(".repo-card3")
+const repo4=document.querySelector(".repo-card4")
 
 const star1=document.querySelector(".stars1")
 const fork1=document.querySelector(".forks1")
@@ -32,8 +32,7 @@ const star4=document.querySelector(".stars4")
 const fork4=document.querySelector(".forks4")
 
 const giturl="https://api.github.com/users/"
-const repourl="https://api.github.com/users/aayushi1403/repos"
-
+const repourl="https://api.github.com/users/"
 
 async function githubfind(username) {
   const response = await fetch(giturl+username)
@@ -45,10 +44,72 @@ async function githubfind(username) {
     var data=await response.json();
     profilepicture.src=data.avatar_url;
     name.innerHTML=data.login;
+    document.querySelector(".Username span").innerHTML=data.name
+    bio.innerHTML=data.bio;
+    repository.innerHTML=data.public_repos;
+
+    
+    userfollowers.innerHTML=data.followers;
+    userfollowing.innerHTML=data.following;
+
+    userlocation.innerHTML=data.location;
+    gitlink.innerHTML=data.url;
+
+    if(data.blog!=""){
+      userblog.innerHTML=data.blog;
+    }
+    else{
+      userblog.innerHTML="no blog available";
+    }
+
+    usertwitter.innerHTML=data.twitter_username || "no twitter available";
   }
 }
 
+async function gitRepos(username){
+  const response1=await fetch(repourl+username+"/repos")
+
+  if(response1.status==404){
+    alert("please put a valid username");
+    return;
+  }
+  else{
+
+    // FIX
+    var data2 = await response1.json();
+
+    if(data2.length>0){
+      repo1.querySelector("h3").innerHTML=data2[0].name
+      repo1.querySelector("p").innerHTML=data2[0].description || "No description"
+      star1.innerHTML = `<i class="fa-solid fa-star"></i> ${data2[0].stargazers_count}`;
+fork1.innerHTML = `<i class="fa-solid fa-code-fork"></i> ${data2[0].forks_count}`;
+    }
+
+    if(data2.length>1){
+      repo2.querySelector("h3").innerHTML=data2[1].name
+      repo2.querySelector("p").innerHTML=data2[1].description || "No description"
+      star2.innerHTML = `<i class="fa-solid fa-star"></i> ${data2[1].stargazers_count}`;
+fork2.innerHTML = `<i class="fa-solid fa-code-fork"></i> ${data2[1].forks_count}`;
+    }
+
+    if(data2.length>2){
+      repo3.querySelector("h3").innerHTML=data2[2].name
+      repo3.querySelector("p").innerHTML=data2[2].description || "No description"
+      star3.innerHTML = `<i class="fa-solid fa-star"></i> ${data2[2].stargazers_count}`;
+fork3.innerHTML = `<i class="fa-solid fa-code-fork"></i> ${data2[2].forks_count}`;
+    }
+
+    if(data2.length>3){
+      repo4.querySelector("h3").innerHTML=data2[3].name
+      repo4.querySelector("p").innerHTML=data2[3].description || "No description"
+      star4.innerHTML = `<i class="fa-solid fa-star"></i> ${data2[3].stargazers_count}`;
+fork4.innerHTML = `<i class="fa-solid fa-code-fork"></i> ${data2[3].forks_count}`;
+    }
+
+  }
+}
 
 searchglass.addEventListener('click',()=>{
-  githubfind(searchbar.value);
+  githubfind(searchbar.value)
+  gitRepos(searchbar.value)
 })
